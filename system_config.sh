c3119c14cb397a849e90d26e83d620c84065e4d6
lcdwiki <lcdwiki@163.com>
2024-10-31 03:54:56 +0000

    modify pi5 bug

--- system_config.sh
+++ system_config.sh
@@ -2,6 +2,7 @@
 
 #get version number
 version=`lsb_release -r | awk -F ' '  '{printf $NF}'`
+hw_result=`tr -d '\0' < /proc/device-tree/model`
 
 #get 32bit or 64bit system
 if [ $(getconf WORD_BIT) = '32' ] && [ $(getconf LONG_BIT) = '64' ] ; then
@@ -10,10 +11,20 @@ else
 hardware_arch=32
 fi
 
+if [[ $hw_result == *"Raspberry Pi 5"* ]]; then
+hardware_model=5
+else
+hardware_model=255
+fi
+
+if [ $hardware_model -eq 5 ]; then
+sudo cp -rf ./boot/config-nomal-PI5.txt ./boot/config.txt.bak
+else
 if [ $hardware_arch -eq 32 ]; then
 sudo cp -rf ./boot/config-nomal-32.txt ./boot/config.txt.bak
 elif [ $hardware_arch -eq 64 ]; then
 sudo cp -rf ./boot/config-nomal-64.txt ./boot/config.txt.bak
+fi
 fi
 
 
